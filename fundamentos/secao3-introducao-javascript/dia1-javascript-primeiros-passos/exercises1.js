00fdd4ca8b510c0406d7ce071a5bc1b79ae64003
Cris Chagas <crischagase@gmail.com>
2023-02-28 14:49:05 -0300

    Resolve exercício 2

--- fundamentos/secao3-introducao-javascript/dia1-javascript-primeiros-passos/exercises1.js
+++ fundamentos/secao3-introducao-javascript/dia1-javascript-primeiros-passos/exercises1.js
@@ -6,3 +6,10 @@ console.log(a - b);
 console.log(a * b);
 console.log(a / b);
 console.log(a % b);
+
+if (a > b) {
+    console.log('A é maior que B');
+}
+else {
+    console.log('B é maior que A');
+}
