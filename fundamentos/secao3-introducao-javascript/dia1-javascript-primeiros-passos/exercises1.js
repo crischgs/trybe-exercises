206f35f9d04819ec4c5b0ed4416309b97312b4eb
Cris Chagas <crischagase@gmail.com>
2023-02-28 14:53:34 -0300

    Resolver eercícios 3, 4 e 5

--- fundamentos/secao3-introducao-javascript/dia1-javascript-primeiros-passos/exercises1.js
+++ fundamentos/secao3-introducao-javascript/dia1-javascript-primeiros-passos/exercises1.js
@@ -1,5 +1,6 @@
 let a = 5;
 let b = 10;
+let c = 20;
 
 console.log(a + b);
 console.log(a - b);
@@ -12,4 +13,36 @@ if (a > b) {
 }
 else {
     console.log('B é maior que A');
+}
+
+if (a > b && a > c) {
+  console.log('O maior número é: ' + a + ' (a)');
+} else if (b > a && b > c) {
+  console.log('O maior número é: ' + b + ' (b)');
+} else {
+  console.log('O maior número é: ' + c + ' (c)');
+}
+
+let number = 4;
+if (number > 0) {
+  console.log('positive');
+} else if (number < 0) {
+  console.log('negative');
+} else {
+  console.log('zero');
+}
+
+let degreeAngleA = 65;
+let degreeAngleB = 100;
+let degreeAngleC = 15;
+let sumOfAngles = degreeAngleA + degreeAngleB + degreeAngleC;
+let allAnglesArePositives = degreeAngleA > 0 && degreeAngleB > 0 && degreeAngleC > 0;
+if(allAnglesArePositives){
+  if (sumOfAngles === 180) {
+    console.log(true);
+  } else {
+    console.log(false);
+  };
+} else {
+  console.log('Erro: ângulo inválido!');
 }
