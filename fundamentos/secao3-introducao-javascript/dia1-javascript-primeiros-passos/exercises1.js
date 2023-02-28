2b2bcd8f2a90cb06aba075b9605604ba5f68bd5a
Cris Chagas <crischagase@gmail.com>
2023-02-28 14:56:31 -0300

    Resolve exercícios finais

--- fundamentos/secao3-introducao-javascript/dia1-javascript-primeiros-passos/exercises1.js
+++ fundamentos/secao3-introducao-javascript/dia1-javascript-primeiros-passos/exercises1.js
@@ -32,17 +32,4 @@ if (number > 0) {
   console.log('zero');
 }
 
-let degreeAngleA = 65;
-let degreeAngleB = 100;
-let degreeAngleC = 15;
-let sumOfAngles = degreeAngleA + degreeAngleB + degreeAngleC;
-let allAnglesArePositives = degreeAngleA > 0 && degreeAngleB > 0 && degreeAngleC > 0;
-if(allAnglesArePositives){
-  if (sumOfAngles === 180) {
-    console.log(true);
-  } else {
-    console.log(false);
-  };
-} else {
-  console.log('Erro: ângulo inválido!');
-}
+
