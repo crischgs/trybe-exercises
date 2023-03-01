5e8cab7d20de9e6da2f68950d8e46f29695c3263
Cris Chagas <crischagase@gmail.com>
2023-03-01 11:23:52 -0300

    Resolve exercício 3

--- fundamentos/secao3-introducao-javascript/dia2-javascript-array-loop-for/exercise3.js
+++ fundamentos/secao3-introducao-javascript/dia2-javascript-array-loop-for/exercise3.js
@@ -1,8 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let result = 0;
+let sum = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
-    result += numbers[index];
+    sum += numbers[index];
 }
 
-console.log(result);
+let average = sum / numbers.length;
+
+console.log(average);
