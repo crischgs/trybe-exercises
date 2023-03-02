230f240c084931991d77bb5578a7d080e1e9cb68
Cris Chagas <crischagase@gmail.com>
2023-03-02 11:30:39 -0300

    Refatora prática 3

--- fundamentos/secao3-introducao-javascript/dia3-javascript-logica-programacao-algoritmos/practice3.js
+++ fundamentos/secao3-introducao-javascript/dia3-javascript-logica-programacao-algoritmos/practice3.js
@@ -1,22 +1,38 @@
 let clientesTrybeBank = ['Ada', 'John', 'Gus'];
 
-function removeCliente(nome) {
-    let clienteExiste = false;
-    if (typeof nome === 'string') {
-        for (let index = 0; index < clientesTrybeBank.length; index += 1) {
-            if (nome === clientesTrybeBank[index]) {
-                clientesTrybeBank.splice(index, 1);
-                clienteExiste = true;
-                return 'Cliente removido com sucesso.';
-            }
-        }
-        if (clienteExiste === false) {
-            return 'Cliente não encontrado.'
-        }
-    } else {
-        return 'O parâmetro deve ser uma string.';
-    }
+function validaCliente(cliente) {
+  if (typeof cliente !== 'string') {
+    return 'O parâmetro passado deve ser do tipo "string"!';
+  } else {
+    return true;
+  }
 }
 
-console.log(removeCliente('John'));
-console.log(clientesTrybeBank);
+function clienteIndex(cliente) {
+  for (let index = 0; index < clientesTrybeBank.length; index += 1) {
+    if (cliente === clientesTrybeBank[index]) {
+      return index;
+    }
+  }
+  return false;
+}
+
+function removeCliente(cliente) {
+  let validacao = validaCliente(cliente);
+  if (validacao !== true) {
+    return validacao;
+  }
+
+  let index = clienteIndex(cliente);
+  if (index === false) {
+    return 'Cliente não encontrada(o).'
+  }
+
+  clientesTrybeBank.splice(index, 1);
+  return 'Cliente excluída(o) com sucesso.';
+}
+
+console.log(removeCliente(false)); // O parâmetro passado deve ser do tipo "string"!
+console.log(removeCliente('Barney')); // Cliente não encontrada(o)
+console.log(removeCliente('John')); // Cliente excluída(o) com sucesso.
+console.log(clientesTrybeBank); // [ 'Ada', 'Gus' ]
