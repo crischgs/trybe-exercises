3c7a706081aeb0fcf17dce5a51a50e27b9444667
Cris Chagas <crischagase@gmail.com>
2023-03-03 14:31:55 -0300

    Inicia resolução do exercício bônus  organizando lições

---
+++ fundamentos/secao3-introducao-javascript/dia4-javascript-objetos-funcoes/trybe-exercise-organizing-lessons.js
@@ -0,0 +1,32 @@
+let lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  let lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  let lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+function addNewKey(object, key, value) {
+    object[key] = value;
+}
+
+addNewKey(lesson2, 'turno', 'noite')
+console.log(lesson2);
+
+function listKeys (object) {
+    console.log(Object.keys(object));
+}
+
+listKeys(lesson3);
