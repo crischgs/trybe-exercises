66521a48937078d314b5e9320179a3c4a0e62e23
Cris Chagas <crischagase@gmail.com>
2023-03-03 00:36:35 -0300

    Primeiros testes com objects em javascript

--- fundamentos/secao3-introducao-javascript/dia4-javascript-objetos-funcoes/practice1-objects.js
+++ fundamentos/secao3-introducao-javascript/dia4-javascript-objetos-funcoes/practice1-objects.js
@@ -57,3 +57,40 @@ console.log(usuario['infoPessoal']['endereco']['cidade']);
 
 ///////////////////////////////////////
 
+let moradores = [
+  {
+    nome: 'Luíza',
+    sobrenome: 'Guimarães',
+    andar: 10,
+    apartamento: 1005,
+  },
+  {
+    nome: 'William',
+    sobrenome: 'Albuquerque',
+    andar: 5,
+    apartamento: 502,
+  },
+  {
+    nome: 'Murilo',
+    sobrenome: 'Ferraz',
+    andar: 8,
+    apartamento: 804,
+  },
+  {
+    nome: 'Zoey',
+    sobrenome: 'Brooks',
+    andar: 1,
+    apartamento: 101,
+  },
+];
+
+let primeiroMorador = moradores[0];
+console.log(primeiroMorador); 
+console.log(primeiroMorador['andar']);
+
+let ultimoMorador = moradores[moradores.length - 1];
+console.log(ultimoMorador);
+console.log(ultimoMorador.nome);
+
+///////////////////////////////////////
+
