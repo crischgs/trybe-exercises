88ebd18dc80b3822d18cf88c1a9863690ce3a028
Cris Chagas <crischagase@gmail.com>
2023-03-03 01:14:16 -0300

    Cria laço for pra contar a quantidade de vezes que Marta foi eleita melhor do mundo e exibe no console

--- fundamentos/secao3-introducao-javascript/dia4-javascript-objetos-funcoes/exercise-about-marta.js
+++ fundamentos/secao3-introducao-javascript/dia4-javascript-objetos-funcoes/exercise-about-marta.js
@@ -11,3 +11,12 @@ let player = {
 console.log('A jogadora', player.firstName, player.lastName, 'tem', player.age, 'anos de idade.');
 
 player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+
+let wins = 0;
+for (let index = 0; index < player['bestInTheWorld'].length; index += 1) {
+    if (player['bestInTheWorld'].length > wins) {
+        wins += 1;
+    }
+}
+
+console.log('A jogadora', player.firstName, player.lastName, 'foi eleita a melhor do mundo', wins, 'vezes.');
