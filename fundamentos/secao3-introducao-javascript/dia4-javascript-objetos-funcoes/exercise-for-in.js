2dc2a17afd4c6f8d2850e6f0c5bfab8c03ccbdf7
Cris Chagas <crischagase@gmail.com>
2023-03-03 01:35:25 -0300

    Imprime no console as chaves e valores do objeto car

--- fundamentos/secao3-introducao-javascript/dia4-javascript-objetos-funcoes/exercise-for-in.js
+++ fundamentos/secao3-introducao-javascript/dia4-javascript-objetos-funcoes/exercise-for-in.js
@@ -7,3 +7,15 @@ let names = {
 for (let key in names) {
     console.log('Olá,', names[key]);
 }
+
+/////////////////////////////
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+for (let key in car) {
+    console.log(key, car[key]);
+}
