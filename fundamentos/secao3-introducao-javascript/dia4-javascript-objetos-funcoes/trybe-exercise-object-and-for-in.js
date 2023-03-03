07df1288994e69b0c0a2a40b5eafb86fa44815de
Cris Chagas <crischagase@gmail.com>
2023-03-03 11:20:53 -0300

    Gera nova info e junta no log

--- fundamentos/secao3-introducao-javascript/dia4-javascript-objetos-funcoes/trybe-exercise-object-and-for-in.js
+++ fundamentos/secao3-introducao-javascript/dia4-javascript-objetos-funcoes/trybe-exercise-object-and-for-in.js
@@ -17,3 +17,18 @@ for (let key in info) {
 for (let key in info) {
     console.log(info[key]);
 }
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+}
+
+for (let key in info) {
+    if (key === 'recorrente' && info[key] === 'Sim' && info2[key] === 'Sim') {
+        console.log('Ambos recorrentes');
+    } else {
+        console.log(info[key], 'e', info2[key]);
+    }
+}
