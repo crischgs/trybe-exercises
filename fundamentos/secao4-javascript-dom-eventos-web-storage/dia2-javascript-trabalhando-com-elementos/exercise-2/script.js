b66634d724238c5d7ddc41382a19c8da8e0812cd
Cris Chagas <crischagase@gmail.com>
2023-03-09 23:49:25 -0300

    Resolve 4

--- fundamentos/secao4-javascript-dom-eventos-web-storage/dia2-javascript-trabalhando-com-elementos/exercise-2/script.js
+++ fundamentos/secao4-javascript-dom-eventos-web-storage/dia2-javascript-trabalhando-com-elementos/exercise-2/script.js
@@ -7,3 +7,11 @@ const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
 const filhoElementoOndeVoceEsta = document.createElement('section');
 filhoElementoOndeVoceEsta.id = 'filhoElementoOndeVoceEsta';
 elementoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta);
+
+const primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
+const filhoDoPrimeiroFilhoDoFilho = document.createElement('section');
+filhoDoPrimeiroFilhoDoFilho.id = 'filhoDoPrimeiroFilhoDoFilho';
+primeiroFilhoDoFilho.appendChild(filhoDoPrimeiroFilhoDoFilho);
+
+const terceiroFilho = filhoDoPrimeiroFilhoDoFilho.parentElement.parentElement.nextElementSibling;
+console.log(terceiroFilho);
