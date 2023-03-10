bd6bed36b106523620947d20082b9469f7977cdc
Cris Chagas <crischagase@gmail.com>
2023-03-10 01:21:19 -0300

    Adiciona tag main

--- fundamentos/secao4-javascript-dom-eventos-web-storage/dia2-javascript-trabalhando-com-elementos/exercise-travel-agency/script.js
+++ fundamentos/secao4-javascript-dom-eventos-web-storage/dia2-javascript-trabalhando-com-elementos/exercise-travel-agency/script.js
@@ -1,3 +1,7 @@
 const headingOne = document.createElement('h1');
 headingOne.innerText = 'TrybeTrip - Agência de Viagens';
 document.body.appendChild(headingOne);
+
+const mainTag = document.createElement('main');
+mainTag.className = 'main-content';
+document.body.appendChild(mainTag);
