6d0982c67c1574383513ffdcf95352fb0167b675
Cris Chagas <crischagase@gmail.com>
2023-03-09 23:41:09 -0300

    Resolve 2

--- fundamentos/secao4-javascript-dom-eventos-web-storage/dia2-javascript-trabalhando-com-elementos/exercise-2/script.js
+++ fundamentos/secao4-javascript-dom-eventos-web-storage/dia2-javascript-trabalhando-com-elementos/exercise-2/script.js
@@ -2,3 +2,8 @@ const pai = document.getElementById('pai');
 const irmaoElementoOndeVoceEsta = document.createElement('section');
 irmaoElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta';
 pai.appendChild(irmaoElementoOndeVoceEsta);
+
+const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+const filhoElementoOndeVoceEsta = document.createElement('section');
+filhoElementoOndeVoceEsta.id = 'filhoElementoOndeVoceEsta';
+elementoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta);
