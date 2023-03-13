96a77557c913382dc3e481e982791d67c7f1554c
Cris Chagas <crischagase@gmail.com>
2023-03-13 19:02:35 -0300

    Resolve outros requisitos

--- fundamentos/secao4-javascript-dom-eventos-web-storage/dia3-javascript-eventos/exercise-1/main.js
+++ fundamentos/secao4-javascript-dom-eventos-web-storage/dia3-javascript-eventos/exercise-1/main.js
@@ -16,28 +16,19 @@ const handleChangeTech = (event) => {
   secondLi.addEventListener('click', handleChangeTech);
   thirdLi.addEventListener('click', handleChangeTech);
 
-// - Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-// com a classe 'tech';
+  input.addEventListener('input', (event) => {
+    const techElement = document.querySelector('.tech');
+    techElement.innerText = event.target.value;
+  });
+  
+  myWebpage.addEventListener('dblclick', () => {
+    window.location.replace('https://bento.me/cris');
+  });
 
-// - Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-// redirecione para alguma página;
-//  - Que tal redirecionar para seu portfólio?
-
-// - Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-// a cor do mesmo;
-
-// Segue abaixo um exemplo do uso de event.target:
-
-
-const resetText = (event) => {
-  // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
-  // O event possui várias propriedades, porém a mais usada é o event.target,
-  // que retorna o objeto que disparou o evento.
-}
-
-firstLi.addEventListener('dblclick', resetText);
-
-// Não precisa passar o parâmetro dentro da callback resetText. O próprio
-// navegador fará esse trabalho por você, não é legal? Desse jeito, o
-// event.target na nossa função retornará o objeto 'firstLi'.
+  myWebpage.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'red';
+  });
+  
+  myWebpage.addEventListener('mouseout', (event) => {
+    event.target.style.color = 'unset';
+  });
