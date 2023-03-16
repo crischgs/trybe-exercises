839c06d37308ded0fa65e5e8e6f9c6b70a2e0217
Cris Chagas <crischagase@gmail.com>
2023-03-16 19:45:15 -0300

    Adiciona preventDefault ao checkbox

--- fundamentos/secao5-html-e-css-forms-flexbox-responsivo/dia1-html-css-forms/prevent-default/script.js
+++ fundamentos/secao5-html-e-css-forms-flexbox-responsivo/dia1-html-css-forms/prevent-default/script.js
@@ -6,3 +6,7 @@ const HREF_LINK = document.querySelector("#href");
 HREF_LINK.addEventListener ('click', (event) => {
     event.preventDefault();
 })
+
+INPUT_CHECKBOX.addEventListener ('click', (event) => {
+    event.preventDefault();
+})
