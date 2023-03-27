a9f80e7f6b18c901e0ead6e90bb1fb959d74fdbb
Cris Chagas <crischagase@gmail.com>
2023-03-27 09:42:43 -0300

    Add sporting goods files

---
+++ fundamentos/secao6-introducao-javascript-es6-testes-unitarios/dia1-fluxo-excecoes-manipulacao-objetos/trybe-sporting-goods/script.js
@@ -0,0 +1,13 @@
+// Primeira parte
+const promo = [
+  {product: 'bola de beach tennis', price: 29.99},
+  {product: 'mochila de trilha 20 litros', price: 120.99},
+  {product: 'capacete de ciclista', price: 180.59},
+  {product: 'toalha compacta de secagem', price: 39.99},
+  {product: 'kit 5 camisetas Dry Fit', price: 99.99},
+  {product: 'caneleiras de peso - 3kg (par)', price: 44.99},
+  {product: 'bola de ginástica e pilates', price: 149.59},
+  {product: 'óculos de natação', price: 18.99},
+  {product: 'capa protetora de prancha de surf', price: 89.59},
+  {product: 'bola de basquete', price: 39.99},
+];
