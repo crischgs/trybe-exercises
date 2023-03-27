caaa393d69c5bc000f68bf8c3e48093db347becb
Cris Chagas <crischagase@gmail.com>
2023-03-27 10:16:35 -0300

    Show the correct strings

--- fundamentos/secao6-introducao-javascript-es6-testes-unitarios/dia1-fluxo-excecoes-manipulacao-objetos/trybe-sporting-goods/script.js
+++ fundamentos/secao6-introducao-javascript-es6-testes-unitarios/dia1-fluxo-excecoes-manipulacao-objetos/trybe-sporting-goods/script.js
@@ -11,3 +11,27 @@ const promo = [
   {product: 'capa protetora de prancha de surf', price: 89.59},
   {product: 'bola de basquete', price: 39.99},
 ];
+
+const button = document.querySelector('#send-button');
+
+button.addEventListener('click', () => {
+  const name = document.querySelector('#name-id').value;
+  const number = document.querySelector('#number-id').value;
+
+  showPromo(name, number);
+});
+
+const showPromo = (name, number) => {
+  const firstText = document.querySelector('#text-initial');
+  const secondText = document.querySelector('#text-final');
+  const productObject = checkNumber(parseInt(number));
+
+  firstText.innerHTML = `Boas-vindas, ${name}!`;
+  secondText.innerHTML = `A promoção do dia é: 
+    ${productObject.product} no valor de R$ ${productObject.price}`;
+}
+
+// Segunda parte
+const checkNumber = (number) => {
+  return promo[number - 1];
+}
