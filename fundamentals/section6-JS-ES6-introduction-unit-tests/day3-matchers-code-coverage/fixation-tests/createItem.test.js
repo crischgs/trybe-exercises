6cb46553bd2da7f1cd8d8170d7a93c57b17c68e6
Cristiano Chagas <124940956+crischgs@users.noreply.github.com>
2023-04-01 23:55:22 -0300

    add first test

--- fundamentals/section6-JS-ES6-introduction-unit-tests/day3-matchers-code-coverage/fixation-tests/createItem.test.js
+++ fundamentals/section6-JS-ES6-introduction-unit-tests/day3-matchers-code-coverage/fixation-tests/createItem.test.js
@@ -1,6 +1,10 @@
 const createItem = require('./createItem');
 describe('a função createItem', () => {
-  it.todo('cria um item válido');
+  it('cria um item válido', () => {
+    const expected = { name: 'banana', unit: 'kg', price: 1.99, quantity: 20 };
+    const actual = createItem('banana', 'kg', 1.99, 20);
+    expect(actual).toEqual(expected);
+  });
   it.todo('utiliza zero como quantidade padrão');
   it.todo('Lança um erro quando não recebe parâmetros');
   it.todo('Lança um erro se o nome do item não é uma string');
