b9e820103fc4b025880db843bf7554853b41589a
Cristiano Chagas <124940956+crischgs@users.noreply.github.com>
2023-04-02 00:02:48 -0300

    add second test

--- fundamentals/section6-JS-ES6-introduction-unit-tests/day3-matchers-code-coverage/fixation-tests/createItem.test.js
+++ fundamentals/section6-JS-ES6-introduction-unit-tests/day3-matchers-code-coverage/fixation-tests/createItem.test.js
@@ -5,7 +5,11 @@ describe('a função createItem', () => {
     const actual = createItem('banana', 'kg', 1.99, 20);
     expect(actual).toEqual(expected);
   });
-  it.todo('utiliza zero como quantidade padrão');
+  it('utiliza zero como quantidade padrão', () => {
+    const expected = 0;
+    const actual = createItem('banana', 'kg', 1.99);
+    expect(actual).toHaveProperty('quantity', expected);
+  });
   it.todo('Lança um erro quando não recebe parâmetros');
   it.todo('Lança um erro se o nome do item não é uma string');
   it.todo('Lança um erro se o preço é negativo');
