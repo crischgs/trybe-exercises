56a3bf7bddc9fef9207e99404e932f257d1a2cf4
Cristiano Chagas <124940956+crischgs@users.noreply.github.com>
2023-04-02 00:06:59 -0300

    add third test

--- fundamentals/section6-JS-ES6-introduction-unit-tests/day3-matchers-code-coverage/fixation-tests/createItem.test.js
+++ fundamentals/section6-JS-ES6-introduction-unit-tests/day3-matchers-code-coverage/fixation-tests/createItem.test.js
@@ -10,7 +10,10 @@ describe('a função createItem', () => {
     const actual = createItem('banana', 'kg', 1.99);
     expect(actual).toHaveProperty('quantity', expected);
   });
-  it.todo('Lança um erro quando não recebe parâmetros');
+  it('Lança um erro quando não recebe parâmetros', () => {
+    expect(() => createItem()).toThrow();
+  });
+
   it.todo('Lança um erro se o nome do item não é uma string');
   it.todo('Lança um erro se o preço é negativo');
   it.todo('Lança um erro se o preço é zero');
