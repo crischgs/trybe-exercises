0153e4251ad8a123657f162c95101cc8411fd9f2
Cristiano Chagas <124940956+crischgs@users.noreply.github.com>
2023-04-03 16:04:48 -0300

    second practive with find

---
+++ fundamentals/section7-JS-higher-order-functions/day1-JS-higher-order-functions-introduction/practices/find-5-letters.js
@@ -0,0 +1,3 @@
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+console.log(names.find((name) => name.length === 5));
--- fundamentals/section7-JS-higher-order-functions/day1-JS-higher-order-functions-introduction/practices/find-numbers.js
+++ fundamentals/section7-JS-higher-order-functions/day1-JS-higher-order-functions-introduction/practices/find-numbers.js
@@ -1,3 +1,3 @@
 const numbers = [19, 21, 30, 3, 45, 22, 15];
 
-console.log(numbers.forEach((number) => number % 3 === 0 && number % 5 === 0));
+console.log(numbers.find((number) => number % 3 === 0 && number % 5 === 0));
