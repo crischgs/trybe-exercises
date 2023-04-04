eb749673c7e9061e0b59e35f064c6d43dda270a0
Cristiano Chagas <124940956+crischgs@users.noreply.github.com>
2023-04-04 13:54:52 -0300

    [E5] final hof game actions

--- fundamentals/section7-JS-higher-order-functions/day1-JS-higher-order-functions-introduction/exercises/game-actions-simulator.js
+++ fundamentals/section7-JS-higher-order-functions/day1-JS-higher-order-functions-introduction/exercises/game-actions-simulator.js
@@ -57,9 +57,26 @@ const mageAttack = (mage) => {
 const gameActions = {
   warriorTurn: (warriorAttack) => {
     const warriorDamage = warriorAttack(warrior);
-    dragon.healthPoints -= warriorDamage;
     warrior.damage = warriorDamage;
+    dragon.healthPoints -= warriorDamage;
   },
+  mageTurn: (mageAttack) => {
+    const mageTurnStats = mageAttack(mage);
+    const mageDamage = mageTurnStats.damageDealt;
+    mage.damage = mageDamage;
+    mage.mana -= mageTurnStats.manaSpent;
+    dragon.healthPoints -= mageDamage;
+  },
+  dragonTurn: (dragonAttack) => {
+    const dragonDamage = dragonAttack(dragon);
+    mage.healthPoints -= dragonDamage;
+    warrior.healthPoints -= dragonDamage;
+    dragon.damage = dragonDamage;
+  },
+  turnResults: () => battleMembers,
 };
 
 gameActions.warriorTurn(warriorAttack);
+gameActions.mageTurn(mageAttack);
+gameActions.dragonTurn(dragonAttack);
+console.log(gameActions.turnResults());
