f68008de30ab59cd010c4187e338ebbab2a9f878
Cristiano Chagas <124940956+crischgs@users.noreply.github.com>
2023-04-20 09:13:19 -0300

    add function to copy the passwaord

--- front-end/section1-frontend-introduction/day1-development-environment/password-generator/src/main.js
+++ front-end/section1-frontend-introduction/day1-development-environment/password-generator/src/main.js
@@ -1,4 +1,5 @@
 import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
 import './style.css';
 
 const passwordBtnEl = document.querySelector('button');
@@ -6,4 +7,5 @@ const displayPasswordEl = document.querySelector('h2');
 passwordBtnEl.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPasswordEl.innerHTML = randomPassword;
+  copy(randomPassword);
 });
