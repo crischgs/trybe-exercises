f0a94c5f550ecb56ef4fa4904fff8c2795776001
crischgs <crischagase@gmail.com>
2023-06-10 11:56:06 -0300

    create component modules

--- front-end/section2-react-introduction/day1-hello-world-on-react/hello-world/src/App.tsx
+++ front-end/section2-react-introduction/day1-hello-world-on-react/hello-world/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Title from './components/title';
+import Modules from './components/modules';
 
 function App() {
 
   return (
     <div>
       <Title />
+      <Modules />
     </div>
   )
 }
---
+++ front-end/section2-react-introduction/day1-hello-world-on-react/hello-world/src/components/modules.tsx
@@ -0,0 +1,12 @@
+function Modules() {
+  return (
+    <div>
+      <h2>Fundamentals</h2>
+      <h2>Front-end</h2>
+      <h2>Back-end</h2>
+      <h2>Computer Science</h2>
+    </div>
+  );
+}
+
+export default Modules;
