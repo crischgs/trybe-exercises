94c9c4feb5301ef870a5b4bc7c21a0f857e0c7f5
crischgs <crischagase@gmail.com>
2023-06-12 19:32:14 -0300

    create the routes

--- front-end/section3-going-deeper-into-react/day1-react-router/coffee-house/src/App.tsx
+++ front-end/section3-going-deeper-into-react/day1-react-router/coffee-house/src/App.tsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import CoffeeList from './pages/coffee-list';
 import './App.css';
 
 function App() {
   return (
-    <>
-
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/coffees" element={ <CoffeeList /> } />
+      </Routes>
+    </div>
   );
 }
 
