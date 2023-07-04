bed382af3e79e747aa82beae191dd3ae7ad17803
crischgs <crischagase@gmail.com>
2023-07-04 03:14:15 -0300

    implements useEffect

--- front-end/3-going-deeper-into-react/3.2-useEffect/iss-tracker/src/App.tsx
+++ front-end/3-going-deeper-into-react/3.2-useEffect/iss-tracker/src/App.tsx
@@ -1,11 +1,26 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import { fetchCoordinates } from "./services";
+
+type Coordinates = {
+  latitude: number;
+  longitude: number;
+};
 
 function App() {
+  const [coordinates, setCoordinates] = useState<Coordinates | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetchCoordinates();
+      setCoordinates({ latitude: data.latitude, longitude: data.longitude });
+    }
+    fetchData();
+  }, []);
 
   return (
-    <>
-
-    </>
+    <div>
+      <h2>International Space Station Location Tracker</h2>
+    </div>
   )
 }
 
